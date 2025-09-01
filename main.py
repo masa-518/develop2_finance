253bf742a6a2788d016e8a23d37a108dfee739e4
Masahiro Inoue <>
2025-09-01 22:44:28 +0900

    Update

---
+++ main.py
@@ -0,0 +1,17 @@
+# main
+
+from tool.print_log import PrintLog
+
+class Main:
+
+    def __init__(self):
+
+        self.__print = PrintLog("Main")
+
+    def run(self):
+        pass
+
+if __name__ == "__main__":
+
+    yet = Main()
+    yet.run()
